9327026b20fe739bfe01ca15721d877440822c9e
Brandon LeBeau <lebebr01@gmail.com>
2019-01-18 09:41:38 -0600

    update

--- Lectures/01-Introduction/bpm19psqf7375_lecture01a.R
+++ Lectures/01-Introduction/bpm19psqf7375_lecture01a.R
@@ -1,7 +1,7 @@
-
+library(tidyverse)
 # to run this script, you will first have to install JAGS from http://mcmc-jags.sourceforge.net
 
-dataIQ = read.csv(file = "iqdata.csv")
+dataIQ = read_csv(file = "iqdata.csv")
 
 #AUTOMATING PACKAGES NEEDED FOR ANALYSES--------------------------------------------------------------------
 haspackage = require("R2jags")
@@ -38,13 +38,13 @@ summary(model1ML)$sigma^2
 # estimation with Bayesian
 
 model01Bayes = function(){
-  
+
   # likelihood
   for (i in 1:n){
     y[i] ~ dnorm(mu, tau)
   }
-  
-  #priors 
+
+  #priors
   mu ~ dnorm(100, 1/2.13^2)
   tau ~ dunif(1/400, 1/200)
   sigma2 = 1/tau
@@ -54,7 +54,7 @@ data = list(y = dataIQ$y, n = nrow(dataIQ))
 
 jags.param = c("mu", "tau", "sigma2")
 
-fit <- jags.parallel(data=data, 
+fit <- jags.parallel(data=data,
                        parameters.to.save=jags.param,
                        n.iter=50000, n.chains=2,n.thin=2,n.burnin=40000,
                        model.file=model01Bayes)
@@ -63,13 +63,13 @@ plot(chain)
 fit
 
 model02Bayes = function(){
-  
+
   # likelihood
   for (i in 1:n){
     y[i] ~ dnorm(mu, tau)
   }
-  
-  #priors 
+
+  #priors
   mu ~ dunif(-10000, 10000)
   tau ~ dunif(1/5000, 100000)
   sigma2 = 1/tau
@@ -79,7 +79,7 @@ data = list(y = dataIQ$y, n = nrow(dataIQ))
 
 jags.param = c("mu", "tau", "sigma2")
 
-fit2 <- jags.parallel(data=data, 
+fit2 <- jags.parallel(data=data,
                      parameters.to.save=jags.param,
                      n.iter=50000, n.chains=2,n.thin=2,n.burnin=40000,
                      model.file=model02Bayes)
@@ -88,13 +88,13 @@ plot(chain2)
 fit2
 
 model03Bayes = function(){
-  
+
   # likelihood
   for (i in 1:n){
     y[i] ~ dnorm(mu, tau)
   }
-  
-  #priors 
+
+  #priors
   mu ~ dunif(-10000, 10000)
   tau ~ dgamma(.01, .01)
   sigma2 = 1/tau
@@ -104,7 +104,7 @@ data = list(y = dataIQ$y, n = nrow(dataIQ))
 
 jags.param = c("mu", "tau", "sigma2")
 
-fit3 <- jags.parallel(data=data, 
+fit3 <- jags.parallel(data=data,
                       parameters.to.save=jags.param,
                       n.iter=50000, n.chains=2,n.thin=2,n.burnin=40000,
                       model.file=model03Bayes)
@@ -115,13 +115,13 @@ fit3
 
 
 model04Bayes = function(){
-  
+
   # likelihood
   for (i in 1:n){
     y[i] ~ dnorm(mu, tau)
   }
-  
-  #priors 
+
+  #priors
   mu ~ dunif(102, 103)
   tau ~ dunif(1/242, 1/238)
   sigma2 = 1/tau
@@ -131,7 +131,7 @@ data = list(y = dataIQ$y, n = nrow(dataIQ))
 
 jags.param = c("mu", "tau", "sigma2")
 
-fit4 <- jags.parallel(data=data, 
+fit4 <- jags.parallel(data=data,
                       parameters.to.save=jags.param,
                       n.iter=50000, n.chains=2,n.thin=2,n.burnin=40000,
                       model.file=model04Bayes)
